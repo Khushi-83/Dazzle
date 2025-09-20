2c7cd120340454b70e6486f9f65511c1a951e4c0
Robot Sir <email.robotsir@gmail.com>
2025-09-20 23:20:15 +0530

    aaded blogRoute

--- components/ui/HeroSection.tsx
+++ components/ui/HeroSection.tsx
@@ -7,7 +7,7 @@ import { gsap } from "gsap";
 const HeroSection = () => {
   const textRef = useRef(null);
   const buttonsRef = useRef(null);
-
+    
   useEffect(() => {
     // Set initial state (prevents hydration mismatch)
     gsap.set(textRef.current, { y: 200, opacity: 0 });
@@ -32,24 +32,24 @@ const HeroSection = () => {
   }, []);
 
   return (
-    <div className="min-h-[150vh] md:min-h-[130vh] flex justify-center  relative py-20">
-      <div>
-        <h1 ref={textRef} className="font-medium text-7xl space-x-0.5">
+    <div className="min-h-[150vh] md:min-h-[130vh] flex justify-center  relative py-20 items-center">
+      <div >
+        <h1 ref={textRef} className="font-medium text-8xl space-x-0.5">
           Style. Staged. Sold
         </h1>
         <div
           ref={buttonsRef}
           className="flex py-8 justify-center gap-10 min-w-1/2 my-4 mx-auto"
         >
-          <button className="flex p-1 h-12 w-[250px] items-center gap-4 bg-[var(--button-primary)] rounded-sm">
-            <span className="arrow bg-[var(--button-secondery)] inline-flex h-full w-1/6 rounded-sm justify-center items-center">
+          <button className="btn-hover flex p-1 h-12 w-[220px] items-center gap-4 bg-[var(--button-primary)] rounded-sm">
+            <span className="arrow bg-[var(--button-secondery)] inline-flex h-full w-1/5 rounded-sm justify-center items-center">
               <ChevronsRight size={20} />
             </span>
             <p className="text-white text-md">Book a Free Consult</p>
           </button>
 
-          <button className="flex p-1 h-12 w-[250px] items-center gap-4 bg-[var(--background)] rounded-sm border border-gray-400">
-            <span className="arrow bg-[var(--button-primary)] inline-flex h-full w-1/6 rounded-sm justify-center items-center">
+          <button className=" btn-hover flex p-1 h-12 w-[220px] items-center gap-4 bg-[var(--background)] rounded-sm border border-gray-400">
+            <span className="arrow bg-[var(--button-primary)] inline-flex h-full w-1/5 rounded-sm justify-center items-center">
               <ChevronsRight size={20} color="#ffffff" />
             </span>
             <p className="text-[var(--foreground)] text-md">Browse Services</p>
