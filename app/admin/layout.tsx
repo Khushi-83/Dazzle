c6fa7bca36e9cdd55eef38df469491afa8b3732d
Robot Sir <email.robotsir@gmail.com>
2025-09-21 16:44:46 +0530

    added prisma and created blog pages

---
+++ app/admin/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+
+import "../globals.css";
+
+
+
+
+export const metadata: Metadata = {
+    title: "Dazzle - Dashboard",
+    description:
+        "Premium architectural staging and design services for luxury properties",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body>
+                <main>{children}</main>
+            </body>
+        </html>
+    );
+}
--- app/api/blogs/route.ts
+++ app/api/blogs/route.ts
@@ -5,3 +5,10 @@ export async function GET(){
       return NextResponse.json({name:"hello"})
 } 
 
+
+
+export async function POST(){
+      return NextResponse.json({name:"hello"})
+} 
+
+// dazzle@786
