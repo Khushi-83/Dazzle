d41574c8a43c275de2e9c1308e9271e258d476ae
Robot Sir <email.robotsir@gmail.com>
2025-09-21 18:28:34 +0530

    blogendpoint

--- app/api/blogs/route.ts
+++ app/api/blogs/route.ts
@@ -1,25 +1,38 @@
-import { NextResponse,NextRequest } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
+import { PrismaClient } from "@prisma/client";
 
 
+const prisma = new PrismaClient()
 
 // fetch all the blogs
-export async function GET(){
-      return NextResponse.json({name:"hello"})
-} 
+export async function GET() {
+      try {
+            const blogs = await prisma.blog.findMany({})
+
+            if (!blogs) {
+                  return NextResponse.json({})
+            }
+           
+            return NextResponse.json(blogs)
+
+      } catch (error) {
+            console.log(error)
+      }
+}
 
 
 // add new blog
-export async function POST(){
-      return NextResponse.json({name:"hello"})
-} 
+export async function POST() {
+      return NextResponse.json({ name: "hello" })
+}
 
 
 // update blog
-export async function PATCH(){
-      return NextResponse.json({name:"hello"})
-} 
+export async function PATCH() {
+      return NextResponse.json({ name: "hello" })
+}
 
 // Delete blog
-export async function DELETE(){
-      return NextResponse.json({name:"hello"})
+export async function DELETE() {
+      return NextResponse.json({ name: "hello" })
 } 
