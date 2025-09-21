61150d1dddf8f7fd70d3a57f7936de4f94ef51bf
Khushi Sinha <ks1393990@gmail.com>
2025-09-22 00:53:59 +0530

    Fix: production errors

--- components/HomeHeader.tsx
+++ components/HomeHeader.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-import React, { use, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { ChevronDown, ChevronsRight } from "lucide-react";
 
 export default function HomeHeader() {
   const pathname = usePathname();
-  const [isServicesOpen, setIsServicesOpen] = useState<Boolean>(false);
-  const [isStickyNav, setStickyNav] = useState<Boolean>(false)
+  const [isServicesOpen, setIsServicesOpen] = useState<boolean>(false);
+  const [isStickyNav, setStickyNav] = useState<boolean>(false)
 
   const navigationItems = [
     { name: "Home", href: "/" },
